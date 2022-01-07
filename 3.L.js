7b816eaf4262840916d8dd9dbf69a554b86e0175
Gayane25 <gayabennu@gmail.com>
2022-01-07 19:23:07 +0400

    feat:added classes for L

--- 3.L.js
+++ 3.L.js
@@ -1,2 +1,32 @@
 'use strict';
 //Liskov Substitution Principle
+
+class Person {}
+
+class Member extends Person {
+  access() {
+    console.log('you have access');
+  }
+}
+class Guest extends Person {
+  isGuest = true;
+}
+class Frontend extends Member {
+  canCreateFrontend() {}
+}
+class Backend extends Member {
+  canCreateBackend() {}
+}
+class PersonFromDifferentCompany extends Guest {
+  access() {
+    throw new Error(
+      'you have not an access, please go to where you came from '
+    );
+  }
+}
+
+function openSecretDoor(person) {
+  person.access();
+}
+console.log(openSecretDoor(new Frontend()));
+//console.log(openSecretDoor(new PersonFromDifferentCompany()));
